8abb3a1a5b78e4e3c37dc196282fe0e6867453d2
HuydDo <huydinhdo@yahoo.com>
2020-04-08 23:51:56 -0600

    Done.

--- src/components/reviews/Review.js
+++ src/components/reviews/Review.js
@@ -5,10 +5,11 @@ class Review extends Component {
   handleOnClick = (event) => {
     this.props.deleteReview(this.props.review.id)
   }
-  
+
   render() {
     const { review } = this.props
-
+    // console.log('review', review)
+    // console.log('this.props', this.props)
     return (
       <div>
         <li>
