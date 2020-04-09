dcdbbaaeb6876914e36b3673626ed941ab2d90c8
thistonyb <this.tonybaker@gmail.com>
2020-04-09 16:11:27 -0700

    Done.

--- src/containers/RestaurantsContainer.js
+++ src/containers/RestaurantsContainer.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react'
-import RestaurantInput from '../components/restaurants/RestaurantInput'
-import Restaurants from '../components/restaurants/Restaurants'
+import React, { Component } from "react";
+import RestaurantInput from "../components/restaurants/RestaurantInput";
+import Restaurants from "../components/restaurants/Restaurants";
+import { connect } from "react-redux";
 
 class RestaurantsContainer extends Component {
-
   render() {
     return (
       <div>
         <RestaurantInput />
         <Restaurants />
       </div>
-    )
+    );
   }
 }
 
-export default RestaurantsContainer
+export default RestaurantsContainer;
