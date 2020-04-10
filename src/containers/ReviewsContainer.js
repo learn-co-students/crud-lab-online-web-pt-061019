1c28ac9b57180881214ad1a17e43cc9dea215c3c
thistonyb <this.tonybaker@gmail.com>
2020-04-09 17:04:13 -0700

    Done.

--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import ReviewInput from "../components/reviews/ReviewInput";
 import Reviews from "../components/reviews/Reviews";
 import { connect } from "react-redux";
+import { addReview, deleteReview } from "../actions/ReviewActions";
 
 class ReviewsContainer extends Component {
   render() {
     return (
       <div>
-        <ReviewInput />
-        <Reviews />
+        <ReviewInput addReview={addReview} />
+        <Reviews deleteReview={deleteReview} reviews={reviews} />
       </div>
     );
   }
