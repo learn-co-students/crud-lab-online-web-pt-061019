ceee61d3e15a2bcd310d898552c920e8f3cc1099
vulcanism <cynomania@gmail.com>
2020-04-10 11:50:13 -0400

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import Restaurant from "./Restaurant";
 
 class Restaurants extends Component {
   render() {
+    const { restaurants, deleteRestaurant } = this.props
+    const restaurantList = restaurants.map((r) => {
+      return (
+        <Restaurant
+          key={r.id}
+          restaurant={r}
+          deleteRestaurant={deleteRestaurant}
+          />
+      );
+    });
     return(
       <ul>
-        Restaurants Component
+        {restaurantList}
       </ul>
     );
   }
--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -2,10 +2,37 @@ import React, { Component } from 'react';
 import Reviews from './Reviews';
 
 class ReviewInput extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      text: ""
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.props.addReview({ text: this.state.text, restaurantId: this.props.restaurantId })
+    this.setState({
+      text: ""
+    })
+  }
+
+
   render() {
     return (
       <div>
-        Review Input
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.handleChange} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
