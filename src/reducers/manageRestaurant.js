4280713a00f76cc600c3153163f2b652bd89078a
thistonyb <this.tonybaker@gmail.com>
2020-04-09 17:37:08 -0700

    Done.

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -5,8 +5,30 @@ export default function manageRestaurants(
   state = { restaurants: [], reviews: [] },
   action
 ) {
-    switch(action.type) {
-        case "ADD_RESTAURANT":
-            return {...state, restaurants:}
-    }
+  switch (action.type) {
+    case "ADD_RESTAURANT":
+      return {
+        ...state,
+        restaurants: [
+          ...state.restaurants,
+          { text: action.text, id: cuidFn() },
+        ],
+      };
+
+    case "DELETE_RESTAURANT":
+      return {
+        ...state,
+        restaurants: state.restaurants.filter(
+          (restaurant) => restaurant.id !== action.id
+        ),
+      };
+
+    case "ADD_REVIEW":
+      const review = {
+        text: action.review.text,
+        id: cuidFn,
+        restaurantId: action.restaurantId,
+      };
+      return { ...state, reviews: [...state.reviews, review] };
+  }
 }
