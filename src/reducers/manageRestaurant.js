0f4d7c5356f0fbda804aa3fb366cce88f0a180c7
scrabill <shannon.crabill@gmail.com>
2020-04-29 14:26:52 -0400

    Done.

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -41,7 +41,7 @@ export default function manageRestaurants(state = {restaurants: [], reviews: []}
     case 'DELETE_REVIEW':
     // console.log(action) // {type: "ADD_REVIEW", text: "hhhhhhhh", restaurantId: "ck9kdp2qo00003h5zt5c8xzr8"}
       console.log("Boop")
-      let reviews = state.reviews.filter(review => review.id !== action.id)
+      const reviews = state.reviews.filter(review => review.id !== action.id)
       console.log(reviews)
       return {
         ...state,
